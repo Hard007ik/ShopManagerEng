5c3c008018b31c93231697b45a6830ed2b5d79da
Hard007ik <hardikmakwana122005@gmail.com>
2026-04-08 16:58:09 +0530

    updated server/app.py as per submit validation

--- server/app.py
+++ server/app.py
@@ -13,3 +13,6 @@ app = create_fastapi_app(JewelryShopEnvironment, JewelryAction, JewelryObservati
 
 def main():
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+if __name__ == "__main__":
+    main()
